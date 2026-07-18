262662694be090c1bf32605d150a0f1bae6ac7c1
Aakash Chavan <97724881+ChavanAakash@users.noreply.github.com>
2026-07-18 14:49:57 +0530

    kk

--- js/click-sound.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("click", () => {
-    const sound = new Audio("media/rclick.mp3"); // path तुमच्या project प्रमाणे बदला
-    sound.volume = 0.4;
-    sound.play().catch(() => {});
-});
